9f001ddf21d632450f1f7e7cae7069ea4c3e504d
Sny Spyper <snuspuper@example.com>
2024-09-09 11:12:26 +0300

    changed wallpaper

--- theme/theme.lua
+++ theme/theme.lua
@@ -84,7 +84,7 @@ theme.titlebar_maximized_button_focus_inactive  = theme_path .. "titlebar/maximi
 theme.titlebar_maximized_button_normal_active   = theme_path .. "titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active    = theme_path .. "titlebar/maximized_focus_active.png"
 
-theme.wallpaper                                 = theme_path .. "spiral_perfecthue_3840x2400.jpg"
+theme.wallpaper                                 = theme_path .. "greenday.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh                              = theme_path .. "layouts/fairhw.png"
